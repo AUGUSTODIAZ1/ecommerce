712ab8a6aca5e6530c8ec37b2ae183b538d3ed92
Marcos Lopez <marcosbenjaminlopez@gmail.com>
2023-09-13 11:38:31 -0300

    cambio carrito y busqueda

---
+++ JS/buscador.js
@@ -0,0 +1,9 @@
+document.addEventListener("keyup", e=> {
+    if (e.target.matches("#buscador")) {
+        document.querySelectorAll(".articulo").forEach(product =>{
+            product.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?product.classList.remove("filtro")
+            :product.classList.add("filtro")
+        })
+    }
+  })
