88dffdebb1d0801085a746e32b275b96602a2c5c
Augusto Diaz <augustodiazgt@gmail.com>
2023-09-13 11:13:45 -0300

    error

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
 // cerrar sesion
 const user = JSON.parse(localStorage.getItem('login_success')) || false
 if (!user) {
-  window.location.href = './index.html'
+  window.location.href = './home.html'
 }
 
 const logout = document.querySelector('#logout')
 logout.addEventListener('click', ()=>{
   alert('Hasta pronto')
   localStorage.removeItem('login_success')
-  window.location.href = './index.html'
+  window.location.href = './home.html'
 });
